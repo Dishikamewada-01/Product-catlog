58000ea8ecb96b38a559ced562ec40025b7901fd
DishikaMewada <163709499+Dishikamewada-01@users.noreply.github.com>
2025-07-24 14:09:28 +0530

    add - Users.java

---
+++ src/main/java/com/ecom/productcatalog/Users.java
@@ -0,0 +1,60 @@
+package com.ecom.productcatalog.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String username;
+	private String email;
+	private String password;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Users() {
+		super();
+		
+	}
+	
+	
+	public Users( String username, String email, String password) {
+		super();
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+	
+	
+
+}
